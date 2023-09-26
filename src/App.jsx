a831c728dc387d22b4e41f15f3a97f0522b00d70
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2023-09-26 22:59:58 +0330

    starred important todos added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TodoHeader from './components/TodoHeader'
 import AddNewTodo from './components/AddNewTodo'
 import TodoFilter from './components/TodoFilter'
@@ -6,11 +6,21 @@ import TodoList from './components/TodoList'
 
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(()=> JSON.parse(localStorage.getItem("todos")) || [])
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
   const handleComplete = (e)=>{
     e.preventDefault();
     const todoId = Number(e.target.id);
     setTodos(prevTodos => prevTodos.map(todo => todo.id == todoId ? {...todo, isCompleted: !todo.isCompleted}: todo))
+  }
+
+  const handleStar = (e)=>{
+    const todoId = Number(e.target.id)
+    setTodos(prevTodos => prevTodos.map(todo => todo.id == todoId ? {...todo, isStared: !todo.isStared}: todo))
   }
 
   return (
@@ -19,7 +29,7 @@ function App() {
       <section className="max-w-sm mx-auto md:max-w-lg lg:max-w-xl xl:max-w-2xl">
           <AddNewTodo setTodos={setTodos}/>
           <TodoFilter/>  
-          <TodoList todos={todos} onComplete={handleComplete}/>
+          <TodoList todos={todos} onComplete={handleComplete} onStar={handleStar}/>
       </section>
     
     </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function TodoList({todos, onComplete}) {
+function TodoList({todos, onComplete, onStar}) {
   return (
     <div>
         <h1 className="text-indigo-400 text-md font-bold">وظایف امروز من:</h1>
         <div className="p-5" id="todo-list__container">
             {
                 todos.map(todo => (
-                    <TodoItem key={todo.id} todo={todo} onComplete={onComplete}/>
+                    <TodoItem key={todo.id} todo={todo} onComplete={onComplete} onStar={onStar}/>
                 ))
             }
         </div>
@@ -17,7 +17,7 @@ function TodoList({todos, onComplete}) {
 
 export default TodoList
 
-function TodoItem({todo, onComplete}){
+function TodoItem({todo, onComplete, onStar}){
     return (
         <div className="bg-indigo-900 hover:bg-indigo-800 transition-all duration-400 ease-out p-2 rounded-xl relative mb-5">
             <div className="flex justify-between"> 
@@ -31,9 +31,9 @@ function TodoItem({todo, onComplete}){
                     <p className={`text-indigo-100 text-sm md:text-md lg:text-lg ${todo.isCompleted ? "line-through opacity-40" : ""}`}>{todo.title}</p>
                 </div>
 
-                <button className="star-btn text-indigo-500 mb-4 transition-all duration-400 ease-in-out">
+                <button id={todo.id} onClick={onStar} className="text-indigo-500 mb-4 transition-all duration-400 ease-in-out">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
-                        className="w-6 h-6 pointer-events-none">
+                        className={`${todo.isStared ? "activeStar" : ""} w-6 h-6 pointer-events-none`}>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                     </svg>  
                 </button>                                                         
