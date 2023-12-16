db029b0ea8448f529399b47f82d40474cd0c5a73
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2023-12-16 20:19:45 +0330

    vercel installed

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import toast from "react-hot-toast";
 
 const api = axios.create({
     baseURL: "http://localhost:5000"
